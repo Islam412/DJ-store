9980cf2729599cdbeaa0f8d9754895c8c45189b7
admin <khmo492007@gmail.com>
2023-07-23 04:26:07 +0300

    add contact back end

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Info
+from django.conf import settings
+from django.core.mail import send_mail
 from django.views.generic import ListView
 # Create your views here.
 
@@ -13,3 +15,23 @@ class contact(ListView):
     model = Info
     template_name = 'contact.html'
     context_object_name = 'info'
+
+
+
+
+def send_massege(request):
+
+    if request.method == "POST":
+        name= request.POST['name']
+        email=request.POST['email']
+        message=request.POST['message']
+        recipient_list = ['masterkhaled33@gmail.com', 'khmo492007@gmail.com', 'khaled.mohamed.tallat@gmail.com']
+        send_mail(
+            name,
+            message,
+            settings.EMAIL_HOST_USER,
+            recipient_list,
+        )
+
+    else:
+        return redirect('/')
