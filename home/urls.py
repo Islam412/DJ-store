208e18d4d679ac32acee42e31c0065f74f076282
admin <khmo492007@gmail.com>
2023-07-23 20:44:11 +0300

    add serch with serch bar

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
 from . import views
 urlpatterns=[
-    path('',views.index,name='home')
+    path('search/', views.product_search_view, name='home'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from shop.models import Product
 
-# Create your views here.
+def product_search_view(request):
+    search_query = request.GET.get('search_query')
+    products = Product.objects.all()
 
-def index(request):
-    return render(request,'home/index.html')
+    if search_query:
+        products = products.filter(Name__icontains=search_query)
+
+    return render(request, 'home/index.html', {'search_query': search_query, 'products': products})
 
 
-from contact.views import contact
+
+
+
+
+
