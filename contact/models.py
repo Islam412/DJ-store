61dc07afb959bf77f87c47ced56fd725d475b571
admin <khmo492007@gmail.com>
2023-08-03 16:15:51 +0300

    remove spaces

--- contact/models.py
+++ contact/models.py
@@ -6,7 +6,5 @@ class Info(models.Model):
     Adrees = models.TextField(max_length=1000)
     Email = models.CharField(max_length=200)
     Phone = models.CharField(max_length=20)
-
-
     def __str__(self):
         return self.Adrees
