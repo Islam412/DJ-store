d1b5c93a50266a15b23e7223ffeecc32656b7f30
admin <khmo492007@gmail.com>
2023-08-03 16:14:40 +0300

    remove spaces

--- shop/models.py
+++ shop/models.py
@@ -4,37 +4,27 @@ from django.contrib.auth.models import User
 from django.conf import settings
 import json
 from account.models import CustomUser
-# Create your models here.
-
-
+#_________________________________________
 class Category(models.Model):
     name = models.CharField(max_length=50, null=True)
     image = models.ImageField(upload_to='category', null=True)
 
     def __str__(self):
         return self.name
-
-
+#____________________________________________________
 class SubCategory(models.Model):
     name = models.CharField(max_length=50, null=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-    
-
+#________________________________________________________
 class Brand(models.Model):
     name = models.CharField(max_length=100)
-
-
     def __str__(self):
         return self.name
+#_____________________________________________________
 class Product(models.Model):
-
-
-    
-
-
     Name = models.CharField(max_length=60)
     Price = models.FloatField()
     Description = models.TextField(max_length=2000)
@@ -43,33 +33,22 @@ class Product(models.Model):
     Color=ColorField(default='#000000')
     Category=models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products",null=True)
     Brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name="Brand_Product",null=True)
-
-
-
-
     def __str__(self):
         return self.Name
-    
-
-
-
+#________________________________________________________
 class Cart(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"Cart for {self.user.name}"
-
-
+#________________________________________________________
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-
     def __str__(self):
         return f"{self.quantity} x {self.product.Name} in Cart for {self.cart}"
-
-
+#_____________________________________________________________________
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
@@ -77,29 +56,22 @@ class Order(models.Model):
     phone = models.CharField(max_length=20)
     email = models.EmailField()
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    cart_items = models.ManyToManyField(CartItem)  # استخدم ManyToManyField لتخزين عناصر سلة التسوق
-
-    # قم بإضافة أية حقول أخرى تحتاجها للطلب
-
+    cart_items = models.ManyToManyField(CartItem)  
     def __str__(self):
         return f"الطلب رقم {self.pk} للسيد {self.name}"
-
     def save_cart_items(self, cart_items):
         self.cart_items.set(cart_items)
-
-
-
+#______________________________________________________________________
 class Wishlist(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"wishlist for {self.user.name}"
-
+#_____________________________________________________________________________
 class wishlistItem(models.Model):
     wishlist = models.ForeignKey(Wishlist, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-
     def __str__(self):
         return f"{self.quantity} x {self.product.Name} in wishlist for {self.wishlist.user.name}"
+#___________________________________________________________________________________________
