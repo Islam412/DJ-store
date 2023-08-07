a323a45b33a2668bd4b281c9152b27e261f8fe86
admin <khmo492007@gmail.com>
2023-08-07 09:46:22 +0300

    fix sherch error

--- home/views.py
+++ home/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from shop.models import Product,Category,SubCategory
-from shop.views import get_cart_items_count  ,   get_wishlist_items_count 
 from django.db.models import Count
+from shop.models import Product, Category, CartItem, wishlistItem
+from django.contrib.auth import get_user_model
 
-'''
 def product_search_view(request):
+    # Get the count of cart items for the current user
+    cart_items_count = CartItem.objects.filter(cart__user=request.user).count()
+
+    # Get the count of wishlist items for the current user
+    WishlistItem = get_user_model()
+    wishlist_items_count = wishlistItem.objects.filter(wishlist__user=request.user).count()
+
     search_query = request.GET.get('search_query')
     category_id = request.GET.get('category', None)
 
     products = Product.objects.all()
+    pro = Product.objects.all()
 
     if search_query:
         products = products.filter(Name__icontains=search_query)
@@ -16,32 +23,7 @@ def product_search_view(request):
     if category_id:
         products = products.filter(Category_id=category_id)
 
-    pro = products[:4]
-
-    context = {
-        'search_query': search_query,
-        'products': products,
-        'pro': pro,
-        'categories': Category.objects.annotate(product_count=Count('products')),
-    }
-
-    return render(request, 'home/index.html', context)
-
-'''
-def product_search_view(request):
-    cart_items_count = get_cart_items_count(request.user)
-    wishlist_items_count = get_wishlist_items_count(request.user)
-    search_query = request.GET.get('search_query')
-    category_id = request.GET.get('category', None)  # Get the category_id from the URL query string
-
-    products = Product.objects.all()[:4]
-    pro = Product.objects.all()[:4]
-
-    if search_query:
-        products = products.filter(Name__icontains=search_query)
-
-    if category_id:
-        products = products.filter(Category_id=category_id)
+    products = products[:4]
 
     context = {
         'search_query': search_query,
@@ -49,7 +31,7 @@ def product_search_view(request):
         'pro': pro,
         'cart_items_count': cart_items_count,
         'wishlist_items_count': wishlist_items_count,
-        'categories':Category.objects.annotate(product_count=Count('products')),
+        'categories': Category.objects.annotate(product_count=Count('products')),
     }
 
     return render(request, 'home/index.html', context)
