88165e766c3d8adf89d9e2e4e72739c32a5dd91d
admin <khmo492007@gmail.com>
2023-08-08 11:09:59 +0300

    fix cart login error

--- home/views.py
+++ home/views.py
@@ -4,18 +4,18 @@ from shop.models import Product, Category, CartItem, wishlistItem
 from django.contrib.auth import get_user_model
 
 def product_search_view(request):
-    # Get the count of cart items for the current user
-    cart_items_count = CartItem.objects.filter(cart__user=request.user).count()
+    cart_items_count = 0
+    wishlist_items_count = 0
 
-    # Get the count of wishlist items for the current user
-    WishlistItem = get_user_model()
-    wishlist_items_count = wishlistItem.objects.filter(wishlist__user=request.user).count()
+    if request.user.is_authenticated:
+        cart_items_count = CartItem.objects.filter(cart__user=request.user).count()
+        wishlist_items_count = wishlistItem.objects.filter(wishlist__user=request.user).count()
 
     search_query = request.GET.get('search_query')
     category_id = request.GET.get('category', None)
 
     products = Product.objects.all()
-    pro = Product.objects.all()
+    pro = Product.objects.all()[:4]
 
     if search_query:
         products = products.filter(Name__icontains=search_query)
@@ -35,3 +35,4 @@ def product_search_view(request):
     }
 
     return render(request, 'home/index.html', context)
+
